39bf911fbf6d067fcb6887e20a2cdcd9cd54de0d
UjjwalNatani <ujjwalnatani10@gmail.com>
2024-03-01 15:03:06 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ export function Home() {
     return (
         <div>
             <div className='background-image1' >
-                <h1>Hi, I'm <br /> Ujjwal Natani</h1>
+                <h1 style={{position: "relative" }}>Hi, I'm <br /> Ujjwal Natani</h1>
                 <div class="loader" style={{ display: 'flex', flexDirection: 'row' }}>
                     <div class="dot"></div>
                     <div class="dot"></div>
